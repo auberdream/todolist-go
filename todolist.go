1c4d867ccb429029f8be220bd5e4ddaa9d64529d
Ellie Wem <ellie.wem@rainbird.ai>
2021-03-01 23:04:40 +0000

    add create endpoint

--- todolist.go
+++ todolist.go
@@ -8,14 +8,27 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"encoding/json"
 )
 
 var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
 
+// TodoItemModel in db (Gorm creates DB table automatically)
 type TodoItemModel struct {
 	ID int `gorm:"primary_key"`
 	Description string 
 	Completed bool
+}
+
+
+// CreateToDo creates to-do item in db
+func CreateToDo(w http.ResponseWriter, r *http.Request) {
+	description := r.FormValue("description")
+	todo := &TodoItemModel{Description: description, Completed: false}
+	db.Create(&todo)
+	result := db.Last(&todo)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result.Value)
 }
 
 // Healthz checks health of endpoint
@@ -39,5 +52,6 @@ func main() {
 	log.Info("Starting to-do list API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
+	router.HandleFunc("/todo", CreateToDo).Methods("POST")
 	http.ListenAndServe(":8000", router)
 }
